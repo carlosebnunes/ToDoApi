8072c48aac71ed28219ad65c69276957c01ab0f9
cadu <carlosebnunes@hotmail.com>
2021-07-23 00:33:27 -0300

    Implementação dos métodos + MongoDB

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const userRouter = require('./routes/users')
+const taskRouter = require('./routes/tasks')
+require('./db/mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, ()=>{
   console.log(`Servidor no ar na porta ${port}`)
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,33 +1,76 @@
 const express = require('express')
-
+const Task = require('../models/tasks')
 const router = new express.Router()
 
-router.post('/tasks', (req,res)=>{
-  res.status(201).send()
+router.post('/tasks', async(req,res)=>{
+  const tasks = new Task(req.body)
+
+  try {
+    await tasks.save()
+    res.status(201).send(tasks)
+  } catch (error) {
+    res.status(400).send(error)
+  }
 })
 
-router.get('/tasks', (req,res)=>{
-  const tasks = [
-      {name: 'tasks'},
-      {name: 'tasks'},
-  ]
-  res.status(200).send(tasks)
+router.get('/tasks', async(req,res)=>{
+  try {
+    const tasks = await Task.find({})
+    res.status(200).send(tasks)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
-router.get('/tasks/:id', (req,res)=>{
-  console.log(req.params.id)
-  const user = {name: 'tasks'}
-  res.status(200).send(tasks)
+router.get('/tasks/:id', async(req,res)=>{
+  const _id = req.params.id
+  try {
+    const tasks = await Task.findById(_id)
+    if (!tasks) {
+      return res.status(404).send()
+    }else{
+      res.status(200).send(tasks)
+    }
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
-router.patch('/tasks/:id', (req,res)=>{
-//Atualização
+router.patch('/tasks/:id', async(req,res)=>{
+//Atualizar
+  const updates = Object.keys(req.body)
+  const allowedUpdate = ["description","completed"]
+  const isValidOperation = updates.every((update)=> allowedUpdate.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({error: 'Esses campos não podem ser atualizados'})
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+    if (!task) {
+      return res.status(400).send()
+    }
+    return res.send(task)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+
   res.send()
 })
 
-router.delete('/tasks/:id', (req,res)=>{
+router.delete('/tasks/:id', async(req,res)=>{
   //Deletar
-    res.send()
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id)
+    if (!task) {
+      return res.status(404).send()
+    }else{
+      res.status(200).send()
+    }
+  } catch (error) {
+    res.status(500).send(error)
+  }
   })
 
 module.exports = router
